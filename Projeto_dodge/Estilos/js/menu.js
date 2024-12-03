5e8c2222c3b8c92f58cb012eec86ccc27a97242e
hailtondan <hailtonpvhdaniel@gmail.com>
2024-12-03 09:09:24 +0000

    Delete Projeto_dodge directory

--- Projeto_dodge/Estilos/js/menu.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.getElementById("menu-button");
-    const menuOptions = document.getElementById("menu-options");
-
-    menuButton.addEventListener("click", () => {
-        menuOptions.classList.toggle("hidden");
-    });
-});
